503fc8df0cbba883a5acec605517f06195f8c71c
Shahrukh <shahrukh1494@gmail.com>
2024-07-26 09:53:22 +0530

    numeric and date values

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -25,6 +25,8 @@ const YoutubeForm = () => {
         },
         phoneNumbers: ["", ""],
         phNumbers: [{ number: "" }],
+        age: 0,
+        dob: new Date(),
       },
   });
   const { errors } = formState;
@@ -135,6 +137,32 @@ const YoutubeForm = () => {
               Add phone number
             </button>
           </div>
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="age">Age</label>
+          <input
+            type="number"
+            id="age"
+            {...register("age", {
+              valueAsNumber: true,
+              required: { value: true, message: "Age is required" },
+            })}
+          />
+          <p className="error">{errors.age?.message}</p>
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="dob">Date of birth</label>
+          <input
+            type="date"
+            id="dob"
+            {...register("dob", {
+              valueAsDate: true,
+              required: { value: true, message: "Date of birth is required" },
+            })}
+          />
+          <p className="error">{errors.dob?.message}</p>
         </div>
 
         <button>Submit</button>
