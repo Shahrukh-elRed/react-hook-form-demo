a2dbf7bee6d551508360d9752e19edf693d69080
Shahrukh <shahrukh1494@gmail.com>
2024-08-27 12:19:56 +0530

    yup integration

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
-import YoutubeForm from "./components/YoutubeForm";
+import { YupYouTubeForm } from "./components/YupYouTubeForm";
+// import YoutubeForm from "./components/YoutubeForm";
 
 function App() {
   return (
     <div>
-      <YoutubeForm />
+      {/* <YoutubeForm /> */}
+      <YupYouTubeForm />
     </div>
   );
 }
