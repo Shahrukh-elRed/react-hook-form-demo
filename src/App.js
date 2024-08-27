4b3f4cb854d7c128d46f89da6627208a1fbbd7d8
Shahrukh <shahrukh1494@gmail.com>
2024-08-27 13:20:46 +0530

    zod integration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
-import { YupYouTubeForm } from "./components/YupYouTubeForm";
+import { ZodYouTubeForm } from "./components/ZodYoutubeForm";
+// import { YupYouTubeForm } from "./components/YupYouTubeForm";
 // import YoutubeForm from "./components/YoutubeForm";
 
 function App() {
   return (
     <div>
       {/* <YoutubeForm /> */}
-      <YupYouTubeForm />
+      {/* <YupYouTubeForm /> */}
+      <ZodYouTubeForm />
     </div>
   );
 }
